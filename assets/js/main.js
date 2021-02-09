5f6056e0b6d9532ef77f1dc38c9d1b3c25e91d78
marina-fp <marina.fdezpas@gmail.com>
2021-02-09 10:56:07 +0100

    Add show image to search results

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,54 @@
 "use strict";
 
+//Elementos que me traigo del HTML
 const inputElement = document.querySelector(".js-input");
-const inputValue = inputElement.value;
 const buttonElement = document.querySelector(".js-button");
 const formElement = document.querySelector(".js-form");
 
+//Arrays de objetos que voy a usar
 let shows = [];
 let favoriteShows = [];
 
-//API
 
+//API
 function getDataFromApi() {
-fetch(`http://api.tvmaze.com//search/shows?q=${inputValue}`)
+fetch(`http://api.tvmaze.com//search/shows?q=${inputElement.value}`)
   .then((response) => response.json())
   .then((data) => {
+    console.log(data);
     shows = data;
     paintShows();
   });
 }
+buttonElement.addEventListener('click', getDataFromApi);
+
 
 //PAINT SHOWS
-
 function paintShows() {
     let htmlCode = '';
     for (let index = 0; index < shows.length; index++) {
-        const name = shows[index].show.name;
-        const image = shows[index].show.image.medium;
-        htmlCode += `<li class="js-show">`;
-        htmlCode += `<p class="js-show-name">${name}</p>`;
-        htmlCode += `<img src="${image}" class="js-show-image" alt="${name} poster" />`
+        let name = shows[index].show.name;
+        let image = shows[index].show.image;
+        htmlCode += `<li class="shows__list--element">`;
+        if (image === null) {
+            htmlCode += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?
+            text=TV" class="show-image" alt="No image available" />`
+        } else {
+        htmlCode += `<img src="${image.medium}" class="show-image" alt="${name} poster" />`
+        }
+        htmlCode += `<h4 class="show-name">${name}</h4>`;
         htmlCode += '</li>';
     }
     const showsList = document.querySelector(".js-shows__list");
     showsList.innerHTML = htmlCode;
 }
-buttonElement.addEventListener('submit'/*o 'click'*/, getDataFromApi);
+
+//
 
 //Evitar que se envíe el formulario de forma predeterminada al presionar 'enter'
 function handleForm(ev) {
     ev.preventDefault();
+    getDataFromApi();
   }
   
 formElement.addEventListener('submit', handleForm);
