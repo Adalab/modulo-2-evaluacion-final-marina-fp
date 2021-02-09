6e48a5e2f216aedd3bc6e0d966592e67dbb18347
marina-fp <marina.fdezpas@gmail.com>
2021-02-09 01:00:35 +0100

    Add fetch to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,44 @@
-'use strict';
+"use strict";
+
+const inputElement = document.querySelector(".js-input");
+const inputValue = inputElement.value;
+const buttonElement = document.querySelector(".js-button");
+const formElement = document.querySelector(".js-form");
+
+let shows = [];
+let favoriteShows = [];
+
+//API
+
+function getDataFromApi() {
+fetch(`http://api.tvmaze.com//search/shows?q=${inputValue}`)
+  .then((response) => response.json())
+  .then((data) => {
+    shows = data;
+    paintShows();
+  });
+}
+
+//PAINT SHOWS
+
+function paintShows() {
+    let htmlCode = '';
+    for (let index = 0; index < shows.length; index++) {
+        const name = shows[index].show.name;
+        const image = shows[index].show.image.medium;
+        htmlCode += `<li class="js-show">`;
+        htmlCode += `<p class="js-show-name">${name}</p>`;
+        htmlCode += `<img src="${image}" class="js-show-image" alt="${name} poster" />`
+        htmlCode += '</li>';
+    }
+    const showsList = document.querySelector(".js-shows__list");
+    showsList.innerHTML = htmlCode;
+}
+buttonElement.addEventListener('submit'/*o 'click'*/, getDataFromApi);
+
+//Evitar que se envíe el formulario de forma predeterminada al presionar 'enter'
+function handleForm(ev) {
+    ev.preventDefault();
+  }
+  
+formElement.addEventListener('submit', handleForm);
