39b78a98ed6ee1d8fc21ce8a56f35b4cf7a90b67
marina-fp <marina.fdezpas@gmail.com>
2021-02-10 10:03:02 +0100

    Add saving favorites in Local Storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ let shows = [];
 let favoriteShows = [];
 
 
-//API
+//Traer datos del API según la búsqueda de la usuaria
 function getDataFromApi() {
 fetch(`http://api.tvmaze.com//search/shows?q=${inputElement.value}`)
   .then((response) => response.json())
@@ -23,33 +23,34 @@ fetch(`http://api.tvmaze.com//search/shows?q=${inputElement.value}`)
 buttonElement.addEventListener('click', getDataFromApi);
 
 
-//Pintar series que coinciden con la búsqueda de la usuaria
+//Pinto series que coinciden con la búsqueda de la usuaria y las escucho (También pinta las favoritas en su apartado)
 function paintShows() {
     let htmlCode = '';
     for (const series of shows) {
-        //PARA BORRAR CUANDO ESTÉ SEGURA DE USAR FOR OF let index = 0; index < shows.length; index++) {
+        //Meto en variables las propiedades de cada objeto que voy a necesitar
         let idShow = series.show.id;
         let nameShow = series.show.name;
         let imageShow = series.show.image;
 
-            //Si es favorita la pinto con una clase más que le dará el estilo
+            //Si es favorita la pinto con una clase más que le dará el estilo en css
             let isFavoriteClass;
             if (isFavoriteShow(series)) {
                 isFavoriteClass = 'fav-mode';
             } else { 
                 isFavoriteClass = ''
             }
-    
+
+        //Pinto la tarjeta de cada serie en la que se incluirá su imagen y su título (+ clase favrorita o no)
         htmlCode += `<li class="shows__list--element ${isFavoriteClass} js-show" id="${idShow}">`;
 
-            //Si no tiene imagen le aplico una por defecto
+            //Pinto la imagen, si no tiene le aplico una por defecto
             const noImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV'
             if (imageShow === null) {
                 htmlCode += `<img src="${noImage}" class="show-image" alt="No image available" />`
             } else {
                 htmlCode += `<img src="${imageShow.medium}" class="show-image" alt="${nameShow} poster" />`
             }
-
+        //Pinto el nombre y cierro la etiqueta de la tarjeta
         htmlCode += `<h4 class="show-name">${nameShow}</h4>`;
         htmlCode += '</li>';
     }
@@ -59,13 +60,25 @@ function paintShows() {
     listenToShows();
 }
 
-//Pinto el array de las series marcadas como favoritas en su correspondiente lista
+//Compruebo si una serie es favorita o no
+function isFavoriteShow(series) {
+    const favoriteFound = favoriteShows.find((favorite) => {
+        return favorite.show.id === series.show.id;
+    });
+    if (favoriteFound === undefined) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+//Pinto el array de las series marcadas como favoritas en su correspondiente lista y la llamo desde la función de pintar general
 function paintFavorites() {
     let htmlCode = '';
     for (const favorite of favoriteShows) {
         let nameFavorite = favorite.show.name;
         let imageFavorite = favorite.show.image;
-        
+
         htmlCode += `<li class="favorites__list--element">`
         const noImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV'
             if (imageFavorite === null) {
@@ -79,19 +92,8 @@ function paintFavorites() {
     favoritesListElement.innerHTML = htmlCode
 }
 
-//Definir si una serie es favorita o no
-function isFavoriteShow(series) {
-    const favoriteFound = favoriteShows.find((favorite) => {
-        return favorite.show.id === series.show.id;
-    });
-    if (favoriteFound === undefined) {
-        return false;
-    } else {
-        return true;
-    }
-}
 
-//Escuchar eventos en las tarjetas de las series
+//Escucho eventos en las tarjetas de las series al clickarlas
 function listenToShows() {
 const cardsElements = document.querySelectorAll('.js-show');
     for (const cardElement of cardsElements) {
@@ -116,6 +118,7 @@ function handleShows(ev) {
     }
     setInLocalStorage();
     paintShows();
+    paintFavorites();
 }
 
 //Guardamos el array de favoritas en el LocalStorage pasándolo antes a string
@@ -124,9 +127,23 @@ function setInLocalStorage() {
     localStorage.setItem('favoriteShows', stringFavoriteShows);
 }
 
+//Traigo las favoritas del LocalStorage haciendo un parse
+function getFromLocalStorage() {
+    const localStorageFavorites = localStorage.getItem('favoriteShows');
+
+    //Si no está vacío (tiene algo) pinto el array de series favoritas
+    if (localStorageFavorites !== null) {
+        const arrayFavorites = JSON.parse(localStorageFavorites);
+        favoriteShows = arrayFavorites;
+        paintFavorites();
+    } else {
+        getDataFromApi();
+    }
+}
+getFromLocalStorage();
+
 //Evitar que se envíe el formulario de forma predeterminada al presionar 'enter'
 function handleForm(ev) {
     ev.preventDefault();
-    //getDataFromApi();
 }  
 formElement.addEventListener('submit', handleForm);
